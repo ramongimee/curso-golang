bc5b4cd1e70009aa248ebb66d215571bb3735f3d
Ramon Gimenes <ramongimee@hotmail.com>
2021-07-27 11:54:51 -0300

    first commit

---
+++ fundamentos/primeiro/primeiro.go
@@ -0,0 +1,26 @@
+// Programas executaveis iniciam pelo pacote main
+package main
+
+/*
+Os códigos em Go são organizados em pacotes
+e para usá-los é necessário declarar um ou vários imports
+*/
+import (
+	"fmt"
+)
+
+//A porta de entrada de um programa Go é a função main
+func main() {
+	fmt.Println("Welcome to GO!")
+	fmt.Println("First")
+	fmt.Println("Program!")
+
+	/*
+		Sobre comentários...
+
+		1) Priorize código legível e faça comentários que agrega valor!
+		2) Evite comentários óbvios
+		3) Durante o curso abuse dos comentários
+	*/
+
+}
